4e0446221067a1b6f41c5f4e1c234d2293b2b083
RonyMG <ponyman97@gmail.com>
2023-01-26 04:13:57 -0700

    xyzz

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/prueba-deploy/',
+  base: '/pruebatecnica.github.io/',
   plugins: [vue()],
   resolve: {
     alias: {
